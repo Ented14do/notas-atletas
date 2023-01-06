6ac003013c7c8e8377aea9c5a2cb19a61bddfcbd
Ented14do <122059269+Ented14do@users.noreply.github.com>
2023-01-06 12:11:27 -0300

    Add files via upload

--- notas-atletas.js
+++ notas-atletas.js
@@ -17,5 +17,24 @@ let atletas = [
  }
 ];
 
-console.log(atletas);
-cu;
+
+
+for (let i = 0; i < atletas.length; i++) {
+  let atleta = atletas[i].nome
+  let n1 = atletas[i].notas
+  let nt = n1
+  let n2 = 0
+  n1 = n1.sort()
+  n1 = n1.toString();
+  nt = nt.slice(1,4)
+  nt.forEach(function(v1){
+
+ return n2 = n2+v1
+  });
+  n2 = n2 / nt.length
+
+  
+console.log("Atleta: " + atleta + "\nNotas Obtidas: " + n1 + "\nMédia Válida: " + n2)
+
+ 
+}
